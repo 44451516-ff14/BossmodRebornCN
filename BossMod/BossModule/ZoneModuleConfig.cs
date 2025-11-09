7099b846b4eefe703585953e6e340bd9c69b6350
x <x@x.com>
2025-11-09 16:04:59 +0800

    update

--- BossMod/BossModule/ZoneModuleConfig.cs
+++ BossMod/BossModule/ZoneModuleConfig.cs
@@ -1,6 +1,6 @@
 ﻿namespace BossMod;
 
-[ConfigDisplay(Name = "全职自动化?-Full duty automation", Order = 6)]
+[ConfigDisplay(Name = "任务 / 副本全自动模式", Order = 6)]
 public sealed class ZoneModuleConfig : ConfigNode
 {
     [PropertyDisplay("加载区域模块所需的最低完成度")]
@@ -12,7 +12,7 @@ public sealed class ZoneModuleConfig : ConfigNode
     [PropertyDisplay("在游戏世界中绘制路径点")]
     public bool ShowWaypoints = false;
 
-    [PropertyDisplay("使用冲刺技能进行导航（Smudge、Elusive Jump, etc）")]
+    [PropertyDisplay("使用冲刺技能进行导航（速涂、回避跳跃, etc）")]
     public bool UseDash = false;
 
     [PropertyDisplay("显示xan调试UI")]
