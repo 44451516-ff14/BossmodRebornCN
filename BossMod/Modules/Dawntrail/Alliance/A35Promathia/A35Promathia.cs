5155e0deb7a43facccfe66313a95957891ca685b
xanunderscore <149614526+xanunderscore@users.noreply.github.com>
2026-05-09 13:38:00 -0400

    fix bug in DNC partner selection code

--- BossMod/Modules/Dawntrail/Alliance/A35Promathia/A35Promathia.cs
+++ BossMod/Modules/Dawntrail/Alliance/A35Promathia/A35Promathia.cs
@@ -23,7 +23,7 @@ public enum AID : uint
     _Ability_WheelOfImpregnability1 = 50323, // Helper->self, no cast, range 13 circle
     _Ability_BastionOfTwilight = 50322, // Boss->self, 2.0+1.0s cast, single-target
     _Ability_PestilentPenance = 50330, // Boss->self, 6.4+0.6s cast, single-target
-    _Ability_BastionOfTwilight1 = 50324, // Helper->self, no cast, range ?-50 donut
+    _Ability_BastionOfTwilight1 = 50324, // Helper->self, no cast, range 13-50 donut
     _Ability_PestilentPenance1 = 50331, // Helper->self, 7.0s cast, range 50 width 50 rect
     _Ability_FleetingEternity1 = 50319, // Boss->self, 3.5+1.0s cast, single-target
     _Spell_Comet = 50337, // Boss->self, 4.5+0.5s cast, single-target
@@ -39,9 +39,9 @@ public enum AID : uint
     _Ability_AuroralDrape = 50355, // 4DF2->self, 7.0s cast, range 7 width 7 rect
     _Ability_EmptySeed = 50349, // 4DF0->self, 5.0s cast, range 10 circle
     _Ability_DeadlyRebirth = 50346, // Boss->self, 5.7+1.3s cast, single-target
-    _Ability_DeadlyRebirth1 = 50348, // Helper->self, 1.3s cast, range 50 circle
+    _Ability_DeadlyRebirth1 = 50348, // Helper->self, 1.3s cast, range 50 circle, adds phase enrage
     _Ability_DeadlyRebirth2 = 50694, // Boss->self, 8.0+2.0s cast, single-target
-    _Ability_DeadlyRebirth3 = 50347, // Helper->self, 2.0s cast, range 50 circle
+    _Ability_DeadlyRebirth3 = 50347, // Helper->self, 2.0s cast, range 50 circle, regular raidwide
     _Ability_EarthboundHeaven = 50333, // Boss->self, 2.0+1.0s cast, single-target
     _Ability_MalevolentBlessing = 50327, // Boss->self, 5.7+0.8s cast, single-target
     _Ability_MalevolentBlessing1 = 50328, // Helper->self, 6.5s cast, range 40 23-degree cone
@@ -89,11 +89,251 @@ public enum TetherID : uint
     _Gen_Tether_chn_nomal01f = 12, // 4DF0->4D66
 }
 
+class EmptySalvation(BossModule module) : Components.RaidwideCast(module, AID._Ability_EmptySalvation);
+class Explosion(BossModule module) : Components.StandardAOEs(module, AID._Ability_Explosion, 16, maxCasts: 4, highlightImminent: true);
+// 58.45 -> 48.59
+class WheelOfImpregnability(BossModule module) : Components.GenericAOEs(module, AID._Ability_WheelOfImpregnability1)
+{
+    DateTime _activation;
+
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor)
+    {
+        if (_activation != default)
+            yield return new(new AOEShapeCircle(13), Module.PrimaryActor.Position, default, _activation);
+    }
+
+    public override void OnEventIcon(Actor actor, uint iconID, ulong targetID)
+    {
+        if ((IconID)iconID == IconID._Gen_Icon_m1001_lockon_c0w)
+            _activation = WorldState.FutureTime(9.9f);
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if (spell.Action == WatchedAction)
+        {
+            NumCasts++;
+            _activation = default;
+        }
+    }
+}
+class BastionOfTwilight(BossModule module) : Components.GenericAOEs(module, AID._Ability_BastionOfTwilight1)
+{
+    DateTime _activation;
+
+    public override IEnumerable<AOEInstance> ActiveAOEs(int slot, Actor actor)
+    {
+        if (_activation != default)
+            yield return new(new AOEShapeDonut(13, 50), Module.PrimaryActor.Position, default, _activation);
+    }
+
+    public override void OnEventIcon(Actor actor, uint iconID, ulong targetID)
+    {
+        if ((IconID)iconID == IconID._Gen_Icon_m1001_lockon_c1w)
+            _activation = WorldState.FutureTime(9.9f);
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if (spell.Action == WatchedAction)
+        {
+            NumCasts++;
+            _activation = default;
+        }
+    }
+}
+class PestilentPenance(BossModule module) : Components.StandardAOEs(module, AID._Ability_PestilentPenance, new AOEShapeRect(50, 25));
+class Comet(BossModule module) : Components.BaitAwayIcon(module, new AOEShapeCircle(6), (uint)IconID._Gen_Icon_tank_lockonae_6m_5s_01t, AID._Spell_Comet1, centerAtTarget: true)
+{
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if ((AID)spell.Action.ID is AID._Spell_Comet1 or AID._Spell_Meteor1)
+            CurrentBaits.Clear();
+    }
+}
+
+class FalseGenesis(BossModule module) : BossComponent(module)
+{
+    DateTime _activation;
+
+    public override void DrawArenaBackground(int pcSlot, Actor pc)
+    {
+        if (_activation == default)
+            return;
+
+        Arena.ZoneRect(Arena.Center + new WDir(0, 13), default(Angle), 6.5f, 6.5f, 6.5f, ArenaColor.SafeFromAOE);
+        Arena.ZoneRect(Arena.Center + new WDir(0, 13).Rotate(120.Degrees()), 120.Degrees(), 6.5f, 6.5f, 6.5f, ArenaColor.SafeFromAOE);
+        Arena.ZoneRect(Arena.Center + new WDir(0, 13).Rotate(-120.Degrees()), -120.Degrees(), 6.5f, 6.5f, 6.5f, ArenaColor.SafeFromAOE);
+    }
+
+    public override void AddHints(int slot, Actor actor, TextHints hints)
+    {
+        if (_activation == default)
+            return;
+
+        var pos = actor.Position;
+        if (!(pos.InRect(Arena.Center + new WDir(0, 13), default(Angle), 6.5f, 6.5f, 6.5f) || pos.InRect(Arena.Center + new WDir(0, 13).Rotate(120.Degrees()), 120.Degrees(), 6.5f, 6.5f, 6.5f) || pos.InRect(Arena.Center + new WDir(0, 13).Rotate(-120.Degrees()), -120.Degrees(), 6.5f, 6.5f, 6.5f)))
+            hints.Add("Go to platform!");
+    }
+
+    public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        if (_activation == default)
+            return;
+
+        var center = Arena.Center;
+        hints.AddForbiddenZone(p => !(p.InRect(center + new WDir(0, 13), default(Angle), 6.5f, 6.5f, 6.5f) || p.InRect(center + new WDir(0, 13).Rotate(120.Degrees()), 120.Degrees(), 6.5f, 6.5f, 6.5f) || p.InRect(center + new WDir(0, 13).Rotate(-120.Degrees()), -120.Degrees(), 6.5f, 6.5f, 6.5f)), _activation);
+    }
+
+    public override void OnMapEffect(byte index, uint state)
+    {
+        if (index == 0x0C && state == 0x00200010)
+            _activation = WorldState.FutureTime(10.7f);
+
+        if (index == 0x0C && state == 0x00020001)
+        {
+            _activation = default;
+            WDir[] corner = [
+                new(3.5f, -6.5f),
+                new(3.5f, -6),
+                new(6, -6),
+                new(6, -3.5f),
+                new(6.5f, -3.5f),
+            ];
+            List<WDir> plat = [.. corner, .. corner.Select(c => c.OrthoR()), .. corner.Select(c => -c), .. corner.Select(c => c.OrthoL())];
+            var platform = plat.Select(p => p + new WDir(0, 13));
+            var combined = Arena.Bounds.Clipper.UnionAll(new(platform), [new(platform.Select(p => p.Rotate(120.Degrees()))), new(platform.Select(p => p.Rotate(-120.Degrees())))]);
+            Arena.Bounds = new ArenaBoundsCustom(20, combined);
+        }
+
+        if (index == 0x0C && state == 0x00080004)
+            Arena.Bounds = new ArenaBoundsCircle(25);
+    }
+}
+
+class MemoryReceptacle(BossModule module) : Components.Adds(module, (uint)OID._Gen_MemoryReceptacle);
+class AuroralDrape(BossModule module) : Components.StandardAOEs(module, AID._Ability_AuroralDrape, new AOEShapeRect(7, 3.5f));
+class EmptyBeleaguer(BossModule module) : Components.StandardAOEs(module, AID._Ability_EmptyBeleaguer, 6, 2);
+// winds: 4 casts, rotates 30 degrees between each
+class WindsOfPromyvion(BossModule module) : Components.GenericRotatingAOE(module)
+{
+    public override void OnEventIcon(Actor actor, uint iconID, ulong targetID)
+    {
+        switch ((IconID)iconID)
+        {
+            case IconID._Gen_Icon_m1001_turning_right01w:
+                Sequences.Add(new(new AOEShapeRect(16, 3), actor.Position, actor.Rotation, -30.Degrees(), WorldState.FutureTime(4.5f), 1.4f, 4));
+                break;
+            case IconID._Gen_Icon_m1001_turning_left01w:
+                Sequences.Add(new(new AOEShapeRect(16, 3), actor.Position, actor.Rotation, 30.Degrees(), WorldState.FutureTime(4.5f), 1.4f, 4));
+                break;
+        }
+    }
+
+    public override void OnEventCast(Actor caster, ActorCastEvent spell)
+    {
+        if ((AID)spell.Action.ID is AID._Ability_WindsOfPromyvion1 or AID._Ability_WindsOfPromyvion3)
+        {
+            NumCasts++;
+            if (Sequences.Count > 0)
+                AdvanceSequence(0, WorldState.CurrentTime);
+        }
+    }
+}
+
+class EmptySeed(BossModule module) : Components.KnockbackFromCastTarget(module, AID._Ability_EmptySeed, 10, shape: new AOEShapeCircle(10))
+{
+    // 29-ish degrees
+    static readonly float SafeConeWidth = MathF.Atan2(6, 3.5f) - FortyFiveRad;
+    const float FortyFiveRad = MathF.PI / 4f;
+
+    Angle PlatformOrientation(WPos p)
+    {
+        if (p.Z > Arena.Center.Z)
+            return default;
+        if (p.X < Arena.Center.X)
+            return -120.Degrees();
+        return 120.Degrees();
+    }
+
+    public override IEnumerable<Source> Sources(int slot, Actor actor)
+    {
+        foreach (var src in base.Sources(slot, actor))
+        {
+            if (IsImmune(slot, src.Activation))
+                continue;
+
+            if (!actor.Position.InCircle(src.Origin, 10))
+                continue;
+
+            var orient = PlatformOrientation(src.Origin);
+            var dir = ((actor.Position - src.Origin).ToAngle() - orient).Normalized().Rad;
+            if (dir < 0)
+                dir += MathF.PI * 2;
+
+            var qu = dir % (MathF.PI / 2f);
+
+            if (MathF.Abs(qu - FortyFiveRad) < SafeConeWidth)
+            {
+                var intersect = Arena.Bounds.IntersectRay(actor.Position - Arena.Center, (actor.Position - src.Origin).Normalized());
+                yield return src with { Distance = MathF.Min(intersect - 0.1f, 10) };
+            }
+            else
+                yield return src;
+        }
+    }
+
+    public override void AddAIHints(int slot, Actor actor, PartyRolesConfig.Assignment assignment, AIHints hints)
+    {
+        // already ensured that the only sources returned are the ones that will actually hit us
+        foreach (var src in Sources(slot, actor))
+        {
+            var center = src.Origin;
+            var orient = PlatformOrientation(center);
+            hints.AddForbiddenZone(p =>
+            {
+                if (!p.InCircle(center, 10))
+                    return false;
+                var dir = ((p - center).ToAngle() - orient).Normalized().Rad;
+                if (dir < 0)
+                    dir += MathF.PI * 2;
+                var qu = dir % (MathF.PI / 2f);
+                return MathF.Abs(qu - FortyFiveRad) >= SafeConeWidth;
+            }, src.Activation);
+        }
+    }
+}
+
+class MalevolentBlessingCone(BossModule module) : Components.StandardAOEs(module, AID._Ability_MalevolentBlessing1, new AOEShapeCone(40, 11.5f.Degrees()));
+class MalevolentBlessingSide(BossModule module) : Components.StandardAOEs(module, AID._Ability_MalevolentBlessing2, new AOEShapeRect(50, 25));
+class PestilentPenanceSkinny(BossModule module) : Components.StandardAOEs(module, AID._Ability_PestilentPenance2, new AOEShapeRect(50, 2.5f));
+class InfernalDeliveranceTower(BossModule module) : Components.CastTowers(module, AID._Ability_InfernalDeliverance1, 4, 6, 8);
+class InfernalDeliverancePuddle(BossModule module) : Components.StandardAOEs(module, AID._Ability_InfernalDeliverance2, 8);
+class MeteorSpread(BossModule module) : Components.SpreadFromIcon(module, (uint)IconID._Gen_Icon_loc06sp_05ak1, AID._Spell_Meteor2, 6, 5.1f);
+
 class A35PromathiaStates : StateMachineBuilder
 {
     public A35PromathiaStates(BossModule module) : base(module)
     {
-        DeathPhase(0, SinglePhase);
+        DeathPhase(0, SinglePhase)
+            .ActivateOnEnter<EmptySalvation>()
+            .ActivateOnEnter<Explosion>()
+            .ActivateOnEnter<WheelOfImpregnability>()
+            .ActivateOnEnter<BastionOfTwilight>()
+            .ActivateOnEnter<PestilentPenance>()
+            .ActivateOnEnter<PestilentPenanceSkinny>()
+            .ActivateOnEnter<Comet>()
+            .ActivateOnEnter<FalseGenesis>()
+            .ActivateOnEnter<MemoryReceptacle>()
+            .ActivateOnEnter<AuroralDrape>()
+            .ActivateOnEnter<EmptyBeleaguer>()
+            .ActivateOnEnter<WindsOfPromyvion>()
+            .ActivateOnEnter<EmptySeed>()
+            .ActivateOnEnter<MalevolentBlessingCone>()
+            .ActivateOnEnter<MalevolentBlessingSide>()
+            .ActivateOnEnter<InfernalDeliveranceTower>()
+            .ActivateOnEnter<InfernalDeliverancePuddle>()
+            .ActivateOnEnter<MeteorSpread>();
     }
 
     private void SinglePhase(uint id)
