dab8b72f484610fb8244b4f643df3542e1079142
HerStolenLight <113465818+HerStolenLight@users.noreply.github.com>
2026-05-10 20:47:38 -0600

    Base Outlines, thanks LTS!

---
+++ BossMod/Modules/Dawntrail/Alliance/A30Shantoto/A30Shantoto.cs
@@ -0,0 +1,7 @@
+﻿namespace BossMod.Dawntrail.Alliance.A30Shantoto;
+
+[ModuleInfo(BossModuleInfo.Maturity.WIP, Contributors = "The Combat Reborn Team", PrimaryActorOID = (uint)OID.ShantottoTheDemon, GroupType = BossModuleInfo.GroupType.CFC, GroupID = 1117u, NameID = 14778u, Category = BossModuleInfo.Category.Alliance, Expansion = BossModuleInfo.Expansion.Dawntrail, SortOrder = 1)]
+public sealed class A30Shantoto(WorldState ws, Actor primary) : BossModule(ws, primary, ArenaCenter, new ArenaBoundsSquare(24f))
+{
+    public static readonly WPos ArenaCenter = new(0f, -720f);
+}
---
+++ BossMod/Modules/Dawntrail/Alliance/A30Shantoto/A30ShantotoStates.cs
@@ -0,0 +1,9 @@
+﻿namespace BossMod.Dawntrail.Alliance.A30Shantoto;
+
+sealed class A30ShantotoStates : StateMachineBuilder
+{
+    public A30ShantotoStates(BossModule module) : base(module)
+    {
+        TrivialPhase();
+    }
+}
